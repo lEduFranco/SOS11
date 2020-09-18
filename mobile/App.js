8256b1b76487776fa7ae9c31edbce9bdcaa27232
lEduFranco <luizinho.eduardo33@gmail.com>
2020-09-18 09:05:43 -0300

    primeiro commit

---
+++ mobile/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
